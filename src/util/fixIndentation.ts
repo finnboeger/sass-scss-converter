528f7e9141f5fbe05ffc5ddfa28ad65ad6bd9f8a
Finn Böger <finnboeger@googlemail.com>
2022-11-22 20:40:01 +0100

    prevent adding newline before comments

--- src/util/fixIndentation.ts
+++ src/util/fixIndentation.ts
@@ -1,18 +1,21 @@
 import { ASTLeafNode, ASTNode } from "sast";
 
 export function fixIndentation(node: ASTNode): void {
-  if (node.type === "block") {
-    const spaces = node.children.filter((child: ASTNode) => child.type === "space") as ASTLeafNode[];
-    const indentationForThisBlock = spaces[0].value.replaceAll("\n", "");
-    spaces.forEach((child: ASTLeafNode) => {
-      child.value = child.value.replaceAll(" ", "") + indentationForThisBlock;
-      if (!child.value.startsWith("\n")) {
-        child.value = `\n${child.value}`;
-      }
-    });
-    const [lastChild] = node.children.slice(-1);
-    if (lastChild.type === "space") {
-      node.children.pop();
+  if (node.type !== "block") {
+    return;
+  }
+  const spaces = node.children.filter(
+    (child: ASTNode, i: number) => child.type === "space" && node.children.at(i + 1)?.type !== "singlelineComment",
+  ) as ASTLeafNode[];
+  const indentationForThisBlock = spaces[0].value.replaceAll("\n", "");
+  spaces.forEach((child: ASTLeafNode) => {
+    child.value = child.value.replaceAll(" ", "") + indentationForThisBlock;
+    if (!child.value.startsWith("\n")) {
+      child.value = `\n${child.value}`;
     }
+  });
+  const [lastChild] = node.children.slice(-1);
+  if (lastChild.type === "space") {
+    node.children.pop();
   }
 }
