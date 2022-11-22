1d5ab072ba546279025ba3b799c879ce70810bb8
Finn Böger <finnboeger@googlemail.com>
2022-11-22 19:44:45 +0100

    fix semicolon positioning

--- src/util/addSemicolon.ts
+++ src/util/addSemicolon.ts
@@ -1,4 +1,4 @@
-import { ASTNode } from "sast";
+import { ASTInternalNode, ASTNode } from "sast";
 
 export function addSemicolon(child: ASTNode): void {
   if (child.type === "atrule") {
@@ -8,7 +8,27 @@ export function addSemicolon(child: ASTNode): void {
     });
   }
 
-  if (child.type === "declarationDelimiter") {
-    child.value = child.value.includes(";") ? child.value : `;${child.value}`;
+  // Add `declarationDelimiter` containing a semicolon behind every node type that requires one.
+  if ("children" in child) {
+    const declarations = child.children.filter((c) =>
+      ["atrule", "declaration", "include"].includes(c.type),
+    ) as ASTInternalNode[];
+    for (const declaration of declarations) {
+      // don't include a semicolon for @include nodes that have a content block.
+      if (declaration.type === "include") {
+        const pruned = child.children.filter(
+          (n) => !["singlelineComment", "declarationDelimiter", "space"].includes(n.type),
+        );
+        const nextContentfulChild = pruned.at(pruned.indexOf(declaration) + 1);
+        if (nextContentfulChild?.type === "block") {
+          continue;
+        }
+      }
+
+      child.children.splice(child.children.indexOf(declaration) + 1, 0, {
+        type: "declarationDelimiter",
+        value: ";",
+      });
+    }
   }
 }
